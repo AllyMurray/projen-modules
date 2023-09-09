58f81e2a7edce5c27e9d3139521a01c2129c03ff
Ally Murray <allymurray88@gmail.com>
2023-09-09 20:58:06 +0100

    ci: :green_heart: attempt to fix CI build by removing npm config

--- src/config/npm-config.ts
+++ src/config/npm-config.ts
@@ -3,8 +3,8 @@ import { TypeScriptProject } from 'projen/lib/typescript';
 export class NpmConfig {
   constructor(project: TypeScriptProject) {
     project.npmrc.addConfig('fund', 'false');
-    project.npmrc.addConfig('legacy-peer-deps', 'true');
-    project.npmrc.addConfig('node-linker', 'hoisted');
-    project.npmrc.addConfig('save-exact', 'true');
+    // project.npmrc.addConfig('legacy-peer-deps', 'true');
+    // project.npmrc.addConfig('node-linker', 'hoisted');
+    // project.npmrc.addConfig('save-exact', 'true');
   }
 }
