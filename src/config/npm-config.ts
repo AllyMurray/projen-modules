b1a6ba588784c8dcb98f427d3256812b4a0eda51
Ally Murray <allymurray88@gmail.com>
2023-09-09 16:22:39 +0100

    ci: :green_heart: set legacy-peer-deps true

--- src/config/npm-config.ts
+++ src/config/npm-config.ts
@@ -3,6 +3,7 @@ import { TypeScriptProject } from 'projen/lib/typescript';
 export class NpmConfig {
   constructor(project: TypeScriptProject) {
     project.npmrc.addConfig('fund', 'false');
+    project.npmrc.addConfig('legacy-peer-deps', 'true');
     project.npmrc.addConfig('node-linker', 'hoisted');
     project.npmrc.addConfig('save-exact', 'true');
   }
