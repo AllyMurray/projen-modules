2a55b2c3a53b58c9e2d9ae1e5f6e6eee842529ab
Ally Murray <allymurray88@gmail.com>
2023-09-11 08:15:34 +0100

    feat: :technologist: add vitest.config.js as a managed file

--- src/config/vitest.ts
+++ src/config/vitest.ts
@@ -1,3 +1,4 @@
+import { SourceCode } from 'projen';
 import { TypeScriptProject } from 'projen/lib/typescript';
 
 export class Vitest {
@@ -7,6 +8,7 @@ export class Vitest {
 
   constructor(private project: TypeScriptProject) {
     this.project.addDevDeps('vitest');
+    this.config();
     this.setTestTask();
   }
 
@@ -14,5 +16,16 @@ export class Vitest {
     this.project.testTask.prependExec('vitest --dir=src', {
       receiveArgs: true,
     });
+  }
+
+  private config() {
+    const config = new SourceCode(this.project, 'vitest.config.js');
+    config.line(`import { defineConfig } from 'vitest/config';`);
+    config.open(`export default defineConfig({`);
+    config.open(`test: {`);
+    config.line(`silent: true,`);
+    config.line(`globals: true,`);
+    config.close(`},`);
+    config.close(`});`);
   }
 }
